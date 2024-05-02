7d7a10635ccb9c319be6cc6be98e0faa49c0b5d1
DivyaniY <divyani.21beitv125@gmail.com>
2024-05-02 18:11:25 +0530

    edited index.js

--- index.js
+++ index.js
@@ -1,57 +1,46 @@
-var express = require("express");
-var bodyParser = require("body-parser");
-var mongoose = require("mongoose");
+var express=require("express")
+var bodyParser=require("body-parser")
+var mongoose=require("mongoose")
 
-const app = express();
+const app=express()
 
-app.use(bodyParser.json());
-app.use(express.static('public'));
+app.use(bodyParser.json())
+app.use(express.static('public'))
 app.use(bodyParser.urlencoded({
-    extended: true
-}));
+    extended:true
+}))
+mongoose.connect('mongodb://localhost:27017/users')
+var db=mongoose.connection
+db.on('error',()=> console.log("Error in connecting to database"))
+db.once('open',()=>console.log("connected to Database"))
+app.post("/contact_form",(req,res)=> {
+    var name= req.body.name
+    var email= req.body.email
+    var phone= req.body.phone
+    var subject= req.body.subject
+    var message= req.body.message
 
-// Use environment variable for MongoDB connection URI
-const mongoURI = process.env.MONGODB_URI;
-
-mongoose.connect(mongoURI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
-
-var db = mongoose.connection;
-db.on('error', () => console.log("Error in connecting to database"));
-db.once('open', () => console.log("Connected to Database"));
-
-app.post("/contact_form", (req, res) => {
-    var name = req.body.name;
-    var email = req.body.email;
-    var phone = req.body.phone;
-    var subject = req.body.subject;
-    var message = req.body.message;
-
-    var data = {
-        "name": name,
-        "email": email,
-        "phone": phone,
-        "subject": subject,
-        "message": message
-    };
-
-    db.collection('users').insertOne(data, (err, collection) => {
-        if (err) {
-            throw err;
+    var data={
+        "name":name,
+        "email":email,
+        "phone":phone,
+        "subject":subject,
+        "message":message
+    }
+    db.collection('users').insertOne(data,(err,collection)=>{
+        if(err){
+         throw err;
         }
-        console.log("Record Inserted successfully");
-    });
+        console.log("Record Inserted successfully")
+    })
+    return res.redirect('success.html')
+})
 
-    return res.redirect('success.html');
-});
-
-app.get("/", (req, res) => {
-    res.set({
-        "Allow-acces-Allow-Origin": '*'
-    });
-    return res.redirect('index.html');
+app.get("/",(req,res) => {
+ res.set({
+    "Allow-acces-Allow-Origin":'*'
+ })
+    return res.redirect('index.html')
 }).listen(3000);
 
-console.log("Listening on port 3000");
+console.log("Listening on port 3000")
