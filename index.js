a8e54c2e2f1134eb54b2354f3eccb30bef1c2375
DivyaniY <divyani.21beitv125@gmail.com>
2024-05-02 18:21:49 +0530

    edited index.js

--- index.js
+++ index.js
@@ -1,45 +1,45 @@
-var express=require("express")
-var bodyParser=require("body-parser")
-var mongoose=require("mongoose")
+var express = require("express")
+var bodyParser = require("body-parser")
+var mongoose = require("mongoose")
 
-const app=express()
+const app = express()
 
 app.use(bodyParser.json())
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({
-    extended:true
+    extended: true
 }))
 mongoose.connect('mongodb://localhost:27017/users')
-var db=mongoose.connection
-db.on('error',()=> console.log("Error in connecting to database"))
-db.once('open',()=>console.log("connected to Database"))
-app.post("/contact_form",(req,res)=> {
-    var name= req.body.name
-    var email= req.body.email
-    var phone= req.body.phone
-    var subject= req.body.subject
-    var message= req.body.message
+var db = mongoose.connection
+db.on('error', () => console.log("Error in connecting to database"))
+db.once('open', () => console.log("connected to Database"))
+app.post("/contact_form", (req, res) => {
+    var name = req.body.name
+    var email = req.body.email
+    var phone = req.body.phone
+    var subject = req.body.subject
+    var message = req.body.message
 
-    var data={
-        "name":name,
-        "email":email,
-        "phone":phone,
-        "subject":subject,
-        "message":message
+    var data = {
+        "name": name,
+        "email": email,
+        "phone": phone,
+        "subject": subject,
+        "message": message
     }
-    db.collection('users').insertOne(data,(err,collection)=>{
-        if(err){
-         throw err;
+    db.collection('users').insertOne(data, (err, collection) => {
+        if (err) {
+            throw err;
         }
         console.log("Record Inserted successfully")
     })
     return res.redirect('success.html')
 })
 
-app.get("/",(req,res) => {
- res.set({
-    "Allow-acces-Allow-Origin":'*'
- })
+app.get("/", (req, res) => {
+    res.set({
+        "Allow-acces-Allow-Origin": '*'
+    })
     return res.redirect('index.html')
 }).listen(3000);
 
